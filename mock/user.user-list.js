3194353b1e2f798c72d7760e7b259ceda0699173
bo.cheng <chen8114@sina.com>
2015-12-10 21:35:52 +0800

    update mock

--- mock/user.user-list.js
+++ mock/user.user-list.js
@@ -1,10 +1,7 @@
 module.exports = {
     error               : false ,
-    "date|1444043199427-14511111111"    : 0 ,   //日期
-    hasSign             : false ,               //是否签到
-    "time|0-7"          : 3 ,                   //第几次连续签到
-    start               : 1444043199427 ,       //用户第一次签到
-    coupons             : [                     //优惠券信息
+    "date|1444043199427-14511111111"    : 0 ,   //ewqewqew
+    coupons             : [                     //xxxxxxxx
         {
             node    : 1 ,
             money   : 5
@@ -18,8 +15,8 @@ module.exports = {
             money   : 25
         }
     ] ,
-    "hasCoupon|10-20"    : [ {                  //用户领取奖励列表
-        "phone|13000000000-18900000000"           : 1 , //手机号码
-        "money|5-20"    : 10                            //金额
+    "xxxxxx|10-20"    : [ {                  //xxxxx
+        "xxxxxxx|13000000000-18900000000"           : 1 , //dsadsa
+        "sssssss|5-20"    : 10                            //ewqewqewq
     } ] 
 }
